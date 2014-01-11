e98e10947b182541ebf1edf8a05167341b3e929f
Pat Allan <pat@freelancing-gods.com>
2014-01-11 11:14:51 +1100

    Starting point

---
+++ Gemfile
@@ -0,0 +1,21 @@
+source 'https://rubygems.org'
+
+gem 'rails', '4.0.1'
+gem 'pg', '0.17.1'
+gem 'mysql2', '0.3.14'
+gem 'unicorn', '4.7.0'
+
+gem 'sass-rails', '~> 4.0.0'
+gem 'uglifier', '>= 1.3.0'
+gem 'jquery-rails'
+gem 'turbolinks'
+
+gem 'thinking-sphinx',
+  git:    'git://github.com/atipugin/thinking-sphinx.git',
+  branch: 'master',
+  ref:    '38d8f9d4782927f2b55ae0bfb92dca52ff6c0ce5'
+
+group :development do
+  gem 'capistrano',       '~> 3.0.1'
+  gem 'capistrano-rails', '~> 1.1.0'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Captest::Application.config.secret_key_base = '1deeb2ddae25651b61489442e0cb3badcf5aed03f8389444eaba999e27ce44e7dc51bc0aa277f733eabcaa63324fab9a9cfda168d96b9ffc7310d3d424dcf6a9'
